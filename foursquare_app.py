a2687a87a4f578efc1b7a3117b3902bbd930b443
Claire Kraft <79609609+ckraft-bot@users.noreply.github.com>
2025-02-02 02:18:22 -0500

    run on paragraph to better paragraph

--- foursquare_app.py
+++ foursquare_app.py
@@ -243,6 +243,7 @@ def main():
         This app aims to help users discover establishments that offer a more accommodating and tolerable experience. 
         While the app is primarily designed for individuals with sensory sensitivities, it is also beneficial for wheelchair users, those with chronic illnesses, neurodivergent individuals, anyone experiencing anxiety, and loved ones of these populations. 
         It is essential that people with disabilities have opportunities to enjoy public spaces, be included in the community, and feel a sense of belonging in society. 
+        
         Currently, there is no such app on the market; this presents a prime opportunity to fill the gap. 
         I invite you to join me in imporving this app for everyone. 
         Look at the "Features" list below to see what features are driving the search results on the "Find" tab and offer feedback on the features (to add, or modify, or omit). 
