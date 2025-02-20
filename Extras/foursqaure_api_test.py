f88a9dd926daf3998220bfc4760a6b275d0b99ca
Claire Kraft <79609609+ckraft-bot@users.noreply.github.com>
2025-02-20 12:50:17 -0500

    testing each endpoint individually

---
+++ Extras/foursqaure_api_test.py
@@ -0,0 +1,52 @@
+import requests
+#---------------------- places
+FOURSQUARE_API_KEY = ""
+
+url = "https://api.foursquare.com/v3/places/search?ll=35.0457219%2C-85.3094883&radius=4827&limit=10"
+headers = {
+    "accept": "application/json",
+    "Authorization": FOURSQUARE_API_KEY
+}
+
+response = requests.get(url, headers=headers)
+print("Places Status Code:", response.status_code)
+# print("Response Text:", response.text)
+
+#----------------------- details
+
+url = "https://api.foursquare.com/v3/places/4b993c6ff964a520536c35e3"
+headers = {
+    "accept": "application/json",
+    "Authorization": FOURSQUARE_API_KEY
+}
+
+response = requests.get(url, headers=headers)
+print("Details Status Code:", response.status_code)
+# print(response.text)
+
+#----------------------- photos
+url = "https://api.foursquare.com/v3/places/4bace4ebf964a520b7163be3/photos?limit=1&sort=NEWEST&classifications=indoor"
+
+headers = {
+    "accept": "application/json",
+    "Authorization": FOURSQUARE_API_KEY
+}
+
+response = requests.get(url, headers=headers)
+
+print("Photo Status Code:", response.status_code)
+# print(response.text)
+
+#----------------------- reviews
+import requests
+
+url = "https://api.foursquare.com/v3/places/4bace4ebf964a520b7163be3/tips?limit=5&fields=text&sort=NEWEST"
+
+headers = {
+    "accept": "application/json",
+    "Authorization": "fsq3WjxLfw81YTLiIkIONlz1sw3NwOezY8aYySRxbjl6OXc="
+}
+
+response = requests.get(url, headers=headers)
+print("Reviews Status Code:", response.status_code)
+# print(response.text)
